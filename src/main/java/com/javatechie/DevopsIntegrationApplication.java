54d43bf5298ea09beb927042e6d445cd40c1b5b3
asthagupta123 <astha.gupta.pmsm@Gmail.com>
2026-05-14 19:02:52 +0100

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to Devops again";
+		return "Welcome to Devops again n again";
 	}
 
 	public static void main(String[] args) {
